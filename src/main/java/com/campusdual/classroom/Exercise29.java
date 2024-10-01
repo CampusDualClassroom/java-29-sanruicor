0719e85961f7d4f8d514288b12ab4febf71f3f99
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-10-01 09:59:00 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,14 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        int dividend = 73, divisor = 0;
+        try {
+            int result = dividend / divisor;
+            System.out.println("Resultado: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("No se puede dividir por cero");
+        } finally {
+            System.out.println("Fin del programa");
+        }
     }
 }
